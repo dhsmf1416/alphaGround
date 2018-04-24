20a606a4e8f2b6bce0848b1d72ca46e95932a2c7
dhsmf1416 <37738452+dhsmf1416@users.noreply.github.com>
2018-04-24 22:26:00 +0900

    first 0.1

---
+++ ALPHAGROUNDS/ALPHA_SERVER/Assets/Scripts/bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class bullet : NetworkBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<player>();
+
+        health.TakeDamage(20);
+        Destroy(gameObject);
+    }
+}
