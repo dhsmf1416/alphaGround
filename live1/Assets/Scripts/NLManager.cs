e499db2880168dd8a3c2862ab43aea51226e7baa
dhsmf1416 <37738452+dhsmf1416@users.noreply.github.com>
2018-04-28 15:21:04 +0900

    live1

---
+++ live1/Assets/Scripts/NLManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+public class NLManager : NetworkManager
+{
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        Debug.Log(NetworkServer.connections.Count);
+        if (NetworkServer.connections.Count >= 3 && networkSceneName == "lobby")
+        {
+            ServerChangeScene("InGame");
+            
+        }
+    }
+    public void AdminButton()
+    {
+        StartServer();
+
+    }
+    public void StartButton()
+    {
+        StartClient();
+    }
+
+}
---
+++ live1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{ 
+    Vector2 velocity;
+    Rigidbody2D myRigidbody;
+// Use this for initialization
+void Start () {
+       myRigidbody = GetComponent<Rigidbody2D>();
+        
+	}
+    public void Move(Vector2 _velocity) {
+        velocity = _velocity;
+    }
+
+    public void FixedUpdate() {
+      myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+      //  maincamera.moveCamera(myRigidbody.position);
+    }
+}
