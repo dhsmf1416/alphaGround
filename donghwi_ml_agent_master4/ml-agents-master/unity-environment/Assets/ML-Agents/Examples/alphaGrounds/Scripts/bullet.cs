d1cc9a9550d1dc1f4478f4126fe99f78ef80a361
dw95kim <dw95kim@kaist.ac.kr>
2018-05-20 18:14:51 +0900

    dw4
    dw4

---
+++ donghwi_ml_agent_master4/ml-agents-master/unity-environment/Assets/ML-Agents/Examples/alphaGrounds/Scripts/bullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public PlayerAgent shooter;
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        var hit = col.gameObject;
+
+        
+
+        if (hit.tag == "tree")
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        if (hit.tag == "player" && hit.GetComponent<PlayerAgent>() != shooter)
+        {
+            PlayerAgent health = hit.GetComponent<PlayerAgent>();
+            health.TakeDamage(15);
+            health.AddReward(-15f);
+            Destroy(gameObject);
+            shooter.AddReward(20f);
+        }
+
+        return;
+        
+    }
+}
